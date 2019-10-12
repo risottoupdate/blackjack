5ec81d1f570cd9e31f43d605b500fbfe8691b962
risottoupdate <freepc.yuki@gmail.com>
2019-10-13 01:01:21 +0900

    player_first_draw_point_view

--- deck.rb
+++ deck.rb
@@ -1,28 +1,35 @@
 class Deck
 
-  # カードの初期設定
+  # カードデッキを作成し、シャッフルまで行う
   def initialize
-
-    @card = {}
+    # デッキの配列を生成
+    @cards = []
 
     mk = ["スペード", "ハート", "ダイア", "クラブ"]
-
     num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q" ,"K"]
 
     mk.each do |mark|
       num.each do |number|
-        # 別のインスタンスを作成し、破壊的操作を回避
-        mark_remember = mark.dup
-        # マーク記号＋ナンバー
-        mark_remember.concat(number)
-        @card.merge!(mark_remember => number)
+        # markとnumberの組み合わせを一つずつcardクラスに渡し、それぞれのcardオブジェクトを生成する
+        card = Card.new(mark, number)
+        # 生成したcardを配列に格納していく
+        @cards << card
       end
     end
 
-    def shuffle
-      puts @card
-    end
+    # デッキをシャッフルする
+    @cards.shuffle!
+  end
 
+  def draw
+    # 配列の最初の要素を抜き出す。最後から抜き出す場合はpop
+    @cards.shift
+  end
+
+  def view
+    @cards.each do |card|
+      puts card.show
+    end
   end
 
 end
